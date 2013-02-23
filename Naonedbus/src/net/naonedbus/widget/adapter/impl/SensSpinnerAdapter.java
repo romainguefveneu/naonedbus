b2a3ae28336dec08856836632decafeeee2fd94b
Romain <romain.guefveneu@gmail.com>
2013-02-23 16:25:36 +0100

    Fixes #15 : Ajout une flèche avant le nom du sens dans la vue Arrêts

--- Naonedbus/src/net/naonedbus/widget/adapter/impl/SensSpinnerAdapter.java
+++ Naonedbus/src/net/naonedbus/widget/adapter/impl/SensSpinnerAdapter.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import net.naonedbus.R;
 import net.naonedbus.bean.Sens;
+import net.naonedbus.utils.SymbolesUtils;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
 public class SensSpinnerAdapter extends SensArrayAdapter implements SpinnerAdapter {
 
@@ -17,6 +19,14 @@ public class SensSpinnerAdapter extends SensArrayAdapter implements SpinnerAdapt
 		super(context, R.layout.list_item_sens_spinner, objects);
 		setTypeface(typeface);
 		setTextColor(textColor);
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		final TextView view = (TextView) super.getView(position, convertView, parent);
+		final Sens sens = getItem(position);
+		view.setText(SymbolesUtils.formatSens(sens.text));
+		return view;
 	}
 
 	@Override
