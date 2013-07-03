f78df1da893a9ff14fc7ff21b4a931224790ecdc
Romain Guefveneu <romain.guefveneu@gmail.com>
2013-07-03 13:56:47 +0200

    Chargement des différents types d'équipements sur la carte.

--- Naonedbus/src/net/naonedbus/fragment/impl/MapFragment.java
+++ Naonedbus/src/net/naonedbus/fragment/impl/MapFragment.java
@@ -1,16 +1,19 @@
 package net.naonedbus.fragment.impl;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import net.naonedbus.NBApplication;
 import net.naonedbus.R;
-import net.naonedbus.bean.Equipement.Type;
+import net.naonedbus.bean.Equipement;
 import net.naonedbus.intent.IIntentParamKey;
 import net.naonedbus.loader.MapLoader;
 import net.naonedbus.loader.MapLoader.MapLoaderCallback;
 import net.naonedbus.map.ToastedMarkerOptionsChooser;
 import net.naonedbus.provider.impl.MyLocationProvider;
 import android.content.SharedPreferences;
+import android.location.Location;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
@@ -21,9 +24,13 @@ import com.actionbarsherlock.app.SherlockFragment;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.SubMenu;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
 import com.twotoasters.clusterkraf.Clusterkraf;
 import com.twotoasters.clusterkraf.InputPoint;
 import com.twotoasters.clusterkraf.MarkerOptionsChooser;
@@ -48,6 +55,8 @@ public class MapFragment extends SherlockFragment implements MapLoaderCallback {
 	private GoogleMap mGoogleMap;
 	private Clusterkraf mClusterkraf;
 	private MapLoader mMapLoader;
+
+	private final Set<Equipement.Type> mSelectedTypes = new HashSet<Equipement.Type>();
 
 	private final MyLocationProvider mLocationProvider;
 
@@ -74,6 +83,13 @@ public class MapFragment extends SherlockFragment implements MapLoaderCallback {
 
 		mPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
+		final Equipement.Type[] types = Equipement.Type.values();
+		for (final Equipement.Type type : types) {
+			if (isLayerPreferenceEnabled(type.getId())) {
+				mSelectedTypes.add(type);
+			}
+		}
+
 		initMap();
 		initClusterkraf();
 
@@ -86,8 +102,15 @@ public class MapFragment extends SherlockFragment implements MapLoaderCallback {
 
 		inflater.inflate(R.menu.activity_map, menu);
 
-		if (mLocationProvider.isProviderEnabled() == false) {
-			menu.findItem(R.id.menu_location).setVisible(false);
+		// if (mLocationProvider.isProviderEnabled() == false) {
+		// menu.findItem(R.id.menu_location).setVisible(false);
+		// }
+
+		final SubMenu filterSubMenu = menu.findItem(R.id.menu_layers).getSubMenu();
+		final Equipement.Type[] types = Equipement.Type.values();
+		for (final Equipement.Type type : types) {
+			final MenuItem item = filterSubMenu.add(MENU_GROUP_TYPES, type.getId(), 0, type.getTitleRes());
+			item.setCheckable(true);
 		}
 
 		// final MenuItem item = filterSubMenu.add(0, MENU_ID_SATELLITE, 0,
@@ -98,19 +121,51 @@ public class MapFragment extends SherlockFragment implements MapLoaderCallback {
 
 	@Override
 	public void onPrepareOptionsMenu(final Menu menu) {
+		final Equipement.Type[] types = Equipement.Type.values();
+		final SubMenu filterSubMenu = menu.findItem(R.id.menu_layers).getSubMenu();
+
+		for (final Equipement.Type type : types) {
+			final MenuItem item = filterSubMenu.findItem(type.getId());
+			item.setChecked(mSelectedTypes.contains(type));
+		}
 
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(final MenuItem item) {
+		if (item.getGroupId() == MENU_GROUP_TYPES) {
+			final Equipement.Type type = Equipement.Type.getTypeById(item.getItemId());
+
+			item.setChecked(!item.isChecked());
+
+			setLayerPreference(type.getId(), item.isChecked());
+
+			if (item.isChecked()) {
+				mSelectedTypes.add(type);
+			} else {
+				mSelectedTypes.remove(type);
+			}
+
+			loadMarkers();
+
+		} else {
+
+		}
 		return super.onOptionsItemSelected(item);
 	}
 
 	private void initMap() {
 		final UiSettings uiSettings = mGoogleMap.getUiSettings();
-		uiSettings.setAllGesturesEnabled(false);
 		uiSettings.setScrollGesturesEnabled(true);
 		uiSettings.setZoomGesturesEnabled(true);
+		uiSettings.setCompassEnabled(true);
+
+		final Location currenLocation = mLocationProvider.getLastKnownLocation();
+		if (currenLocation != null) {
+			final CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(
+					currenLocation.getLatitude(), currenLocation.getLongitude()), 15);
+			mGoogleMap.animateCamera(cameraUpdate);
+		}
 	}
 
 	private void initClusterkraf() {
@@ -122,14 +177,49 @@ public class MapFragment extends SherlockFragment implements MapLoaderCallback {
 			// customize the options before you construct a Clusterkraf instance
 			mClusterkraf = new Clusterkraf(mGoogleMap, mOptions, null);
 
-			mMapLoader = new MapLoader(getActivity(), this);
-			mMapLoader.execute(Type.TYPE_BICLOO, Type.TYPE_MARGUERITE, Type.TYPE_PARKING);
+			loadMarkers();
 		}
+	}
+
+	private void loadMarkers() {
+		synchronized (mClusterkraf) {
+			mClusterkraf.clear();
+		}
+
+		final Equipement.Type[] types = mSelectedTypes.toArray(new Equipement.Type[mSelectedTypes.size()]);
+		mMapLoader = new MapLoader(getActivity(), this);
+		mMapLoader.execute(types);
+	}
+
+	/**
+	 * Indique si le calque est activé ou non dans les préférences.
+	 * 
+	 * @param id
+	 * @return Vrai si le calque est activé.
+	 */
+	private boolean isLayerPreferenceEnabled(final Integer id) {
+		if (id == Equipement.Type.TYPE_ARRET.getId()) {
+			return mPreferences.getBoolean(PREF_MAP_LAYER + id, true);
+		} else {
+			return mPreferences.getBoolean(PREF_MAP_LAYER + id, false);
+		}
+	}
+
+	/**
+	 * Changer la valeur d'activation d'un calque.
+	 * 
+	 * @param id
+	 * @param enabled
+	 */
+	private void setLayerPreference(final Integer id, final boolean enabled) {
+		mPreferences.edit().putBoolean(PREF_MAP_LAYER + id, enabled).commit();
 	}
 
 	@Override
 	public void onLayerLoaded(final ArrayList<InputPoint> result) {
-		mClusterkraf.addAll(result);
+		synchronized (mClusterkraf) {
+			mClusterkraf.addAll(result);
+		}
 	}
 
 }
