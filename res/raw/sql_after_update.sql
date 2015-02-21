a9f7146d17ed3b0a59b6c647f350436e7138e3f7
Romain Guefveneu <romain.guefveneu@gmail.com>
2015-02-21 10:37:44 +0100

    Correction de la suppression des favoris dont l'arrêt n'existe plus après une mise à jour.

--- res/raw/sql_after_update.sql
+++ res/raw/sql_after_update.sql
@@ -1,6 +1,7 @@
 INSERT INTO favoris SELECT _id, codeLigne, codeSens, codeArret, nomFavori FROM favoris_backup;
 DROP TABLE favoris_backup;
 
+DELETE FROM favoris WHERE _id IN (SELECT favoris._id FROM favoris LEFT JOIN arrets ON arrets.code = favoris.codeArret AND arrets.codeSens = arrets.codeSens AND favoris.codeLigne = arrets.codeLigne WHERE arrets.code IS NULL);
 UPDATE favoris SET _id = (SELECT arrets._id FROM arrets WHERE arrets.code = favoris.codeArret AND arrets.codeSens = favoris.codeSens AND arrets.codeLigne = favoris.codeLigne);
 DELETE FROM favoris WHERE NOT EXISTS (SELECT 1 FROM lignes WHERE code = codeLigne);
 DELETE FROM favoris WHERE _id IN (SELECT favoris._id FROM favoris LEFT JOIN arrets ON arrets._id = favoris._id LEFT JOIN equipements ON equipements.idType = 0 AND equipements._id = arrets.idStation WHERE codeEquipement IS NULL);
